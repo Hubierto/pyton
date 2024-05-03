070d0a72f50f0141e01d34db85042b89b3ce559b
Roberto <161033388+Hubierto@users.noreply.github.com>
2024-05-03 12:24:17 +0000

    Python  - aula 1.tipos de dados

---
+++ 1.tipo de dados/1.Olá mundo.py
@@ -0,0 +1,9 @@
+# isso é um comentário de uma linha.
+
+'''
+isso é um 
+comentário de 
+múltiplas linhas.
+'''
+
+print("Olá mundos")
---
+++ 1.tipo de dados/2. Tipagem dinâmica.py
@@ -0,0 +1,16 @@
+#Tipagem dinâmica
+
+nome = "Marta"
+idade = 20
+peso = 62.500
+
+#Exibindo dados.
+#simples
+print(nome)
+print(idade)
+print(peso)
+
+#Formatado
+print(f"Nome : {nome}")
+print(f"idade : {idade}")
+print(f"peso : {peso}")
