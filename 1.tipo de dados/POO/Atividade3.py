e04d527e80e9e0f6aafe47daabf43ef2f2b2ba7a
Roberto <161033388+Hubierto@users.noreply.github.com>
2024-09-05 14:44:13 +0000

    a

---
+++ 1.tipo de dados/POO/Atividade3.py
@@ -0,0 +1,32 @@
+from abc import ABC, abstractmethod
+import os
+
+os.system("cls || clear")
+
+class Funcionario(ABC):
+
+    def __init__(self, nome: str, telefone: str, email: str, endereco: Endereco ) -> None:
+       self.nome = nome 
+       self.telefone = telefone
+       self.email = email
+       self.endereco = endereco 
+
+    def __str__(self) -> str:
+        return (
+            f"\nNome: {self.nome}"
+            f"\nTelefone: {self.telefone}" 
+            f"\nE-mail: {self.email}" 
+            f"\nEndereço: {self.endereco}" 
+        )
+
+
+class Endereco:
+    def __init__(self, logradouro: str, numero: str, complemento: str, cep: str, cidade: str) -> None:
+        self.logradouro = logradouro
+        self.numero = numero
+        self.complemento = complemento
+        self.cep = cep
+        self.cidade = cidade
+
+   
+   
