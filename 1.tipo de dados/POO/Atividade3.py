ea44add2fb9664a146fdbc6cce450fe75a6a5a48
Roberto <161033388+Hubierto@users.noreply.github.com>
2024-09-17 13:21:22 +0000

    ok

--- 1.tipo de dados/POO/Atividade3.py
+++ 1.tipo de dados/POO/Atividade3.py
@@ -3,22 +3,6 @@ import os
 
 os.system("cls || clear")
 
-class Funcionario(ABC):
-
-    def __init__(self, nome: str, telefone: str, email: str, endereco: Endereco ) -> None:
-       self.nome = nome 
-       self.telefone = telefone
-       self.email = email
-       self.endereco = endereco 
-
-    def __str__(self) -> str:
-        return (
-            f"\nNome: {self.nome}"
-            f"\nTelefone: {self.telefone}" 
-            f"\nE-mail: {self.email}" 
-            f"\nEndereço: {self.endereco}" 
-        )
-
 
 class Endereco:
     def __init__(self, logradouro: str, numero: str, complemento: str, cep: str, cidade: str) -> None:
@@ -36,9 +20,71 @@ class Endereco:
             f"\nCep: {self.cep}"
             f"\nCidade: {self.cidade}"
 
+        ) 
+    
+class Funcionario(ABC):
+
+    def __init__(self, nome: str, telefone: str, email: str, endereco: Endereco ) -> None:
+       self.nome = nome 
+       self.telefone = telefone
+       self.email = email
+       self.endereco = endereco 
+
+    def __str__(self) -> str:
+        return (
+            f"\nNome: {self.nome}"
+            f"\nTelefone: {self.telefone}" 
+            f"\nE-mail: {self.email}" 
+            f"\nEndereço: {self.endereco}" 
+        )
+    
+    #@abstractmethod
+    #def salarioFinal(ABC) -> float:
+    #    pass
+
+class Engenheiro:
+
+    #BONIFICACAO = 1.5
+
+    def __init__(self, crea: float) -> None:
+        self.crea = crea
+
+    #def salarioFinal(self) -> float:
+    #    resultado = self.salario * self.BONIFICACAO
+    #    return resultado
+
+    def __str__(self) -> float:
+        return(
+            f"\nCrea: {self.crea}"
+        )
+    
+class Medico:
+
+    #BONIFICACAO = 2.0
+
+    def __init__(self, crm: float) -> None:
+        self.crm = crm
+
+    #def salarioFinal(self) -> float:
+    #    resultado = self.salario * self.BONIFICACAO
+    #    return resultado
+
+    def __str__(self) -> float:
+        return(
+            f"\nCrm: {self.crm}"
         )    
 
-        
+funcionario = Funcionario("JJ","71966554433", "JJ@gmail.com", Endereco("...", "07", "E", "345123456", "Salvador"))
+
+engenheiro = Engenheiro(1000)
+
+medico = Medico(2000)
+
+print(funcionario)
+print(engenheiro)
+print(medico)
+
+ 
 
 
 
